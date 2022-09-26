aa18ff82248bf3f536168f0825b02ddd77a8689a
koechronix <koechronix@yahoo.com>
2022-09-26 14:50:40 +0300

    commit tree b41c40ffcce032eded7b1008760875c58fec9ec4

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+    # validates :title, presence : true
+    # validates :category, inclusion: { in: %w[Fiction Non-Fiction]}
+    # validates :content, length: { minimum: 100 }
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
